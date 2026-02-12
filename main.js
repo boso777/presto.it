4c5df6cb1eda3f454bc5b9c59a1e63f264868e34
Boso Mac <robertoingrao4@gmail.com>
2026-02-12 18:49:28 +0100

    presto.it version.1.0

--- main.js
+++ main.js
@@ -4,59 +4,154 @@ let logoNavbar = document.getElementById(`logoNavbar`);
 let lightsaber = document.getElementById(`lightsaber`);
 let check1 = false;
 let collapse = document.getElementById(`collapse`);
+let firstNumber = document.getElementById(`firstNumber`);
+let secondNumber = document.getElementById(`secondNumber`);
+let thirdNumber = document.getElementById(`thirdNumber`);
 
-window.addEventListener(`scroll`, ()=> {
-    
+
+
+window.addEventListener(`scroll`, () => {
     let scrolled = window.scrollY;
-    if(scrolled > 0){
+    if (scrolled > 0) {
         navbar.classList.remove(`bg-black`);
-        navbar.classList.add(`bg-yellow`); 
+        navbar.classList.add(`bg-yellow`);
         collapse.classList.remove(`bg-black`);
         collapse.classList.add(`bg-yellow`);
         navbar.style.height = `140px`;
-        links.forEach( (link) => {
-            link.style.color = 'var(--black)';  
-        })
+        links.forEach((link) => {
+            link.style.color = "var(--black)";
+        });
         logoNavbar.src = `./media/logo black.png`;
-        lightsaber.src = `./media/spada lase.png`
-        
-    }else{
-        
+        lightsaber.src = `./media/spada lase.png`;
+    } else {
         navbar.classList.remove(`bg-yellow`);
         navbar.classList.add(`bg-black`);
         collapse.classList.remove(`bg-yellow`);
         collapse.classList.add(`bg-black`);
         navbar.style.height = `70px`;
-        links.forEach( (link) => {
-            link.style.color = 'var(--yellow)';  
-        })
+        links.forEach((link) => {
+            link.style.color = "var(--yellow)";
+        });
         logoNavbar.src = `./media/logo yellow.png`;
-        lightsaber.src = `./media/SPADA LASER YELLOW.png`
+        lightsaber.src = `./media/SPADA LASER YELLOW.png`;
+    }
+});
+
+lightsaber.addEventListener(`click`, () => {
+    if (check1 == false) {
+        lightsaber.style.transform = `rotate(-90deg)`;
+        check1 = true;
+    } else {
+        lightsaber.style.transform = `rotate(0deg)`;
+        check1 = false;
+    }
+});
+
+// funzione per contare
+
+function Counter(el, n, time) {
+    let counter = 0;
+    
+    let count = setInterval(() => {
+        if (counter < n) {
+            counter++;
+            el.innerHTML = counter;
+        } else {
+            clearInterval(count);
+        }
+    }, time);
+    
+    setTimeout(()=> {
+        confirm = true;
+    }, 8000)
+}
+
+let confirm = true;
+
+//dentro la variabile obsever si sta generando un oggetto di classe IntersectioObserver()
+
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting && confirm) { 
+            Counter(firstNumber, 100, 100);
+            Counter(secondNumber, 200, 50);
+            Counter(thirdNumber, 300, 20);
+            confirm = false;
+        }
+    });
+});
+
+observer.observe(firstNumber);
+
+
+
+let reviews = [
+    {user: `Matteo`, description: `Il più bel sito del mondo`, rank: 5},
+    {user: `Alin`, description: `Schifo`, rank: 1},
+    {user: `Michael`, description: `Mi piace ma odio Star Trek`, rank: 3},
+    {user: `Arina`, description: `Odioso!`, rank: 2},
+]
+
+let swiperWrapper = document.querySelector(`.swiper-wrapper`)
+
+
+reviews.forEach((review) => {
+    
+    let div1 = document.createElement(`div`);
+    
+    div1.classList.add(`swiper-slide`);
+    
+    div1.innerHTML = `<div class="swiper-slide">
+              <div class="card-review">
+              <p class="h4 text-center">${review.user}</p>
+              <p class="lead text-center">${review.description}</p>
+              <div class="d-flex justify-content-center stars">
+                  
+                </div>
+              </div>
+            </div>`;
+    
+    swiperWrapper.appendChild(div1);
+});
+
+let stars = document.querySelectorAll(`.stars`);
+
+// <i class="fa-solid fa-star"></i>
+
+stars.forEach((star, index) => {
+    for(let i = 0; i < reviews[index].rank; i++){
+        let icon = document.createElement(`i`);
+        icon.classList.add(`fa-solid`, `fa-star`);
+        star.appendChild(icon);
+    }
+    
+    let difference = 5 - reviews[index].rank
+    
+    for(let i = 0; i < difference; i++){
+        let icon = document.createElement(`i`);
+        icon.classList.add(`fa-regular`, `fa-star`);
+        star.appendChild(icon);
     }
 })
 
 
+// swipers
 
 
-lightsaber.addEventListener(`click`, ()=> {
+const swiper = new Swiper('.swiper', {
     
-    if(check1 == false){
-        lightsaber.style.transform = `rotate(-90deg)`
-        check1 = true;
-    }else{
-        lightsaber.style.transform = `rotate(0deg)`
-        check1 = false;    
-    }
+    effect: "flip",
+    grabCursor: true,
+    
+    // Optional parameters
+    loop: true,
+    
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    
 });
 
-let counter = 0;
 
-let interval = setInterval( ()=>{
-    if(counter < 100){
-        counter++
-        console.log(counter)
-    }else{
-        console.log(`adesso mi fermo`);
-        clearInterval(interval);
-    }
-}, 1);
